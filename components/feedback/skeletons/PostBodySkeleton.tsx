3d3ff19dcf05ec63121ec4b2776e8a07835428fd
Vladimir Jankovic <vladimir@oysterlink.com>
2024-07-26 01:41:59 +0200

    Post skeleton improved

--- components/feedback/skeletons/PostBodySkeleton.tsx
+++ components/feedback/skeletons/PostBodySkeleton.tsx
@@ -1,3 +1,5 @@
+import { postConfig, socialShareConfig } from "@/models/config";
+import { centerVerticalClass } from "@/styles/global";
 import { postsImageWrapperClass } from "@/styles/layouts";
 import { Skeleton, Stack } from "@mui/material";
 import Box from "@mui/material/Box";
@@ -7,9 +9,21 @@ export default function PostBody({ numberOfItems = 10 }: { numberOfItems?: numbe
 
   return (
     <Stack component="article" spacing={4}>
-      <Box sx={postsImageWrapperClass}>
-        <Skeleton variant="rectangular" width="100%" height="400px" />
-      </Box>
+      {postConfig.postType === "plain-post" ? (
+        <Box sx={postsImageWrapperClass}>
+          <Skeleton variant="rectangular" width="100%" height="400px" />
+        </Box>
+      ) : null}
+      {socialShareConfig.speedDialAlwaysOpen ? (
+        <Stack direction="row" spacing={2} sx={centerVerticalClass}>
+          <Skeleton variant="circular" width={60} height={60} />
+          <Skeleton variant="circular" width={40} height={40} />
+          <Skeleton variant="circular" width={40} height={40} />
+          <Skeleton variant="circular" width={40} height={40} />
+        </Stack>
+      ) : (
+        <Skeleton variant="circular" width={60} height={60} />
+      )}
       <Stack spacing={4}>
         {skeletons.map((skeleton) => {
           return <Skeleton key={skeleton} variant="rectangular" width="100%" height="30px" />;
